ec6b12a5d61e1f638ecd59a592c9132e9524a797
Thomas Brandon <tsbrandon1010@gmail.com>
2023-10-05 00:14:44 -0400

    structural changes

---
+++ main/APICaller.py
@@ -0,0 +1,13 @@
+class ApiCallerInterface:
+    def generate_prompt():
+        pass
+    
+    def query():
+        pass
+    
+    
+
+    pass
+
+def test():
+    print('hello world')
---
+++ test/MockApiTests/main/MockAPI_tests.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent.parent.parent))
+from main import APICaller
+
+
+
+
+APICaller.test()
